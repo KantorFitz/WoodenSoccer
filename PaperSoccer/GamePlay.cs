49de7963b536d19842a3c802f55a72fcf99c1276
Krzysztof Jakubowski <fakedperson@o2.pl>
2021-06-15 19:22:49 +0200

    Game settings initial draw 2

---
+++ PaperSoccer/GamePlay.cs
@@ -0,0 +1,14 @@
+﻿namespace GameSettings
+{
+    enum GamePlay : ushort
+    {
+        NotStarted = 0,
+        Move,
+        Bounce,
+        EndedMove,
+        PositionConfirmed,
+        CanNotEndMove,
+        Draw,
+        FinishedMove
+    }
+}
